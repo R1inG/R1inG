1f567e454456948ad99120106990c39c2a252afc
zyh <142578568+R1inG@users.noreply.github.com>
2024-10-28 17:10:13 +0800

    Update loan_function.h

--- loan_function.h
+++ loan_function.h
@@ -102,16 +102,46 @@ int ShelveNumber(Queue* q) {
         current = current->next; // 移动到下一个节点
     }
     int count = 0;
-    // 打印所有找到的货架ID
-    printf("队列中所有物品所在货架ID:");
+    //printf("队列中所有物品所在货架ID:");
     for (int i = 1; i < 8; i++) {
         if(locateShelve[i]==1){
-            printf("%d ", i);
+            //printf("%d ", i);
             count++;
         }
     }
-    printf("\n");
+    //printf("\n");
     return count;
+}
+
+int *ShelveManager(Queue *q){
+    
+    int locateShelve[8]={0}; // 存储货架ID
+    Item items[100]; // 假设这是从文件中读取的物品列表
+    int number = readItemsFromFile(items, 100); // 读取物品信息
+    QueueNode* current = q->front; // 使用临时指针遍历队列
+    while (current != NULL) {
+        for (int i = 0; i < number; i++) {
+            // 使用 strcmp 进行字符串比较
+            if (strcmp(items[i].item_name, current->item_name) == 0) {
+                locateShelve[items[i].shelve_id] = 1; // 找到对应货架ID
+                break; // 找到后可提前退出内层循环
+            }
+        }
+        current = current->next; // 移动到下一个节点
+    }
+    int count = 0;
+    int *index=(int *)malloc(sizeof(int));
+    //printf("队列中所有物品所在货架ID:");
+    for (int i = 1; i < 8; i++) {
+        if(locateShelve[i]==1){
+            //printf("%d ", i);
+            index[count]=i;
+            printf("%d",index[count]);
+            count++;
+        }
+    }
+    //printf("\n");
+    return index;
 }
 
 // 保存队列到文本中
